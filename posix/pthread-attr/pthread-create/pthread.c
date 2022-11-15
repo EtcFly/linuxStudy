3dd3c258b152067175caa3cb0c78c7e5462013c7
etcfly <etcflywy@126.com>
2022-11-15 10:13:54 +0800

    线程创建属性相关接口

--- posix/pthread-attr/pthread-create/pthread.c
+++ posix/pthread-attr/pthread-create/pthread.c
@@ -1,9 +1,9 @@
 /**
  * @file pthread.c
  * @author mingyang (mingyang.wang@tuya.com)
- * @brief  pthread线程接口测试
+ * @brief  pthread线程属性接口测试
  * @version 1.0.0
- * @date 2022-10-30
+ * @date 2022-11-15
  *
  * @copyright Copyright (c) 2022
  *
@@ -17,18 +17,27 @@
 #define _GNU_SOURCE
 
 /**
- * @brief 线程创建和销毁相关接口
- * pthread_attr_init: 线程创建
- * pthread_attr_destroy： 线程退出
- * pthread_attr_getdetachstate： 线程同步等待子线程状态
- * pthread_attr_setdetachstate: 线程分离, 子线程资源由操作系统回收
- * pthread_attr_getstack： 获取当前线程自身
- * pthread_attr_setstack：线程比对
- * pthread_attr_getstacksize: 线程退出信号
- * pthread_attr_setstacksize: 线程清理push
- * pthread_attr_getguardsize: 线程清理弹出
- * pthread_attr_setguardsize:
+ * @brief 线程属性相关设置接口
+ * pthread_attr_init: 线程属性设为默认值
+ * pthread_attr_destroy： 线程属性销毁
+ * pthread_attr_getdetachstate： 获取线程分离状态
+ * pthread_attr_setdetachstate: 设置线程分离状态
+ * pthread_attr_getstack： 获取线程堆栈(pthread_attr_getstackaddr已经废弃)
+ * pthread_attr_setstack：设置线程堆栈(pthread_attr_setstackaddr已经废弃)
+ * pthread_attr_getstacksize: 获取线程堆栈大小
+ * pthread_attr_setstacksize: 设置线程堆栈大小
+ * pthread_attr_getguardsize: 获取警戒位置大小
+ * pthread_attr_setguardsize: 设置警戒位置大小(堆栈溢出警戒)
  */
+
+#define EXEC_RETURN_ZERO(x)              \
+    do                                   \
+    {                                    \
+        if (!(x))                        \
+        {                                \
+            printf("%s [FAIL]\r\n", #x); \
+        }                                \
+    } while (0)
 
 void show_pthread_info(const char *str)
 {
@@ -60,86 +69,79 @@ void *new_thread(void *arg)
     // return (void *)199; //使用return 清理函数不会被调用
 }
 
-void *old_thread(void *arg)
+void show_detach_state(const pthread_attr_t *stat)
 {
-    pthread_t *tid = (pthread_t *)arg;
-
-    printf("old thread start! tid:%lX\r\n", *tid);
-    pthread_cleanup_push(show_clean_cb, "old thread clean 1");
-    pthread_cleanup_push(show_clean_cb, "old thread clean 2");
-    printf("old thread complete!\r\n");
-
-    if (pthread_equal(*tid, pthread_self()) != 0)
+    int detachState;
+    EXEC_RETURN_ZERO(0 == pthread_attr_getdetachstate(stat, &detachState)); //获取默认状态
+    if (detachState == PTHREAD_CREATE_DETACHED)
     {
-        printf("this is thread self!\r\n");
+        printf("get detach state!\r\n");
     }
     else
     {
-        printf("this diff thread is %lX\r\n", pthread_self());
+        printf("get joinable state!\r\n");
     }
-
-    while (1)
-    {
-        sleep(1);
-        printf("wait sleep to cancel!\r\n");
-    }
-
-    pthread_cleanup_pop((int long)arg);
-    pthread_cleanup_pop((int long)arg);
-
-    printf("thread to cancel finish!\r\n");
-    return (void *)455;
 }
 
+void show_stack_size(pthread_attr_t *attr)
+{
+    size_t stacksize;
+    EXEC_RETURN_ZERO(0 == pthread_attr_getstacksize(attr, &stacksize));
+    printf("get stack size:%ld\r\n", stacksize);
+}
+
+void show_stack_addr(const pthread_attr_t *attr)
+{
+    void *addr;
+    size_t size;
+    EXEC_RETURN_ZERO(0 == pthread_attr_getstack(attr, &addr, &size));
+    printf("get stack address:%p size:%ld\r\n", addr, size);
+}
+
+void show_guard_size(const pthread_attr_t *attr)
+{
+    size_t size;
+    EXEC_RETURN_ZERO(0 == pthread_attr_getguardsize(attr, &size));
+    printf("get guard size:%ld\r\n", size);
+}
+
+static char buffer[800 * 1024] __attribute__((aligned(32)));
 int main(int argc, char *argv[])
 {
+
     pthread_t tid;
     pthread_attr_t init_attr;
 
-    pthread_attr_init(&init_attr);
+    EXEC_RETURN_ZERO(0 == pthread_attr_init(&init_attr));
+
+    show_detach_state(&init_attr);                                    //显示默认值
+    pthread_attr_setdetachstate(&init_attr, PTHREAD_CREATE_DETACHED); //如果系统不设置detach, 然后有没有join等待, 线程会存在栈泄漏的风险
+    show_detach_state(&init_attr);                                    //显示设置值
+
+    show_stack_size(&init_attr); //显示默认堆栈大小
+    pthread_attr_setstacksize(&init_attr, 8000 * 1024ul);
+    show_stack_size(&init_attr);
+
+    show_guard_size(&init_attr);
+    pthread_attr_setguardsize(&init_attr, 1 * 1024); //默认会页对齐
+    show_guard_size(&init_attr);
+
+     show_stack_addr(&init_attr); //接口被弃用
+     pthread_attr_setstack(&init_attr, &buffer[0], sizeof(buffer));
+     printf("stack buffer address:%p\r\n", buffer);
+     show_stack_addr(&init_attr);
 
     if (0 != pthread_create(&tid, &init_attr, new_thread, (void *)1))
     {
-        perror("can't creat threda!");
+        pthread_attr_destroy(&init_attr);
+        perror("can't creat thread!");
         exit(0);
     }
 
-    static pthread_t tid1;
-    if (0 != pthread_create(&tid1, NULL, old_thread, (void *)&tid1))
-    {
-        perror("can't creat threda!");
-        exit(0);
-    }
+    pthread_attr_destroy(&init_attr); // init attr是按值传递, 这里可以销毁
 
     sleep(2);
     show_pthread_info(__FUNCTION__);
-    // if (pthread_cancel(tid) != 0)
-    // {
-    //     perror("thread cancel!");
-    // }
-
-    void *ret;
-    if (0 != pthread_detach(tid))
-    {
-        perror("pthread detach fail!");
-        exit(-1);
-    }
-
-    sleep(2);
-
-    int v = 0;
-    if ((v = pthread_cancel(tid1)) != 0) //被控线程必须内部存在系统调用点
-    {
-        printf("thread tid to cancel fail! %s\r\n", strerror(v));
-        exit(-1);
-    }
-
-    if (0 != pthread_join(tid1, &ret))
-    {
-        printf("thread join err!\r\n");
-        exit(-1);
-    }
-    printf("ret value:%ld %ld\r\n", (long int)ret, (long int)PTHREAD_CANCELED);
 
     printf("Down!\r\n");
     pthread_exit(0);
