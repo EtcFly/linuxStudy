59d266be709926beaddac0fb9e14834d47e86063
etcfly <etcflywy@126.com>
2023-11-19 21:21:59 +0800

    添加共享内存以及同步相关

--- posix/ipc/shareMem/shareMem_recv.c
+++ posix/ipc/shareMem/shareMem_recv.c
@@ -12,14 +12,97 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 
+static int __sem_init(int recv_flag)
+{
+#define SEM_BASE_PATH "../sem/semrcv.c"
+#define SEM_BASE_ID (87654321)
+    int semid;
+    key_t semkey = ftok(SEM_BASE_PATH, SEM_BASE_ID);
+    if (semkey == -1) {
+        perror("semkey get fail");
+        return -1;
+    }
+
+    if (!recv_flag) {
+        semid = semget(semkey, 1, IPC_CREAT | 0666);
+        if (semid < 0) {
+            perror("semget fail");
+            return -1;
+        }
+
+        union semun
+        {
+            int val;
+            struct semid_ds *buf;
+            unsigned short int *array;
+            struct seminfo *__buf;
+        } set;
+        set.val = 0;
+        int ret = semctl(semid, 0, SETVAL, set);
+        if (ret < 0) {
+            perror("semctl fail");
+            return -1;
+        }
+    } else {
+        semid = semget(semkey, 1, 0666);
+        if (semid < 0) {
+            perror("semget fail");
+            return -1;
+        }
+    }
+
+    return semid;
+}
+
+static int __sem_wait(int semid)
+{
+    struct sembuf buf = {
+        .sem_flg = 0,
+        .sem_num = 0,
+        .sem_op = -1,
+    };
+    int ret = semop(semid, &buf, 1);
+    if (ret < 0) {
+        perror("semop wait fail");
+        return -1;
+    }
+    return 0;
+}
+
+static int __sem_post(int semid)
+{
+    struct sembuf buf = {
+        .sem_flg = 0,
+        .sem_num = 0,
+        .sem_op = 1,
+    };
+    int ret = semop(semid, &buf, 1);
+    if (ret < 0) {
+        perror("semop wait fail");
+        return -1;
+    }
+    return 0;
+}
+
+static void __sem_get_value(int semid)
+{
+    int semvalue = semctl(semid, 0, GETVAL);
+    if (semvalue < 0) {
+        perror("semvalue get fail");
+        return ;
+    }
+    printf("semvalue is %d\r\n", semvalue);
+}
 
 #define MSG_BASE_PATH "./shareMem_send.c"
-#define MSG_BASE_ID (12345678)
-#define MAX_SHM_SIZE (4096)
+#define MSG_BASE_ID (123456791)
+#define MAX_SHM_SIZE (4)
 
 int main(int argc, char *argv[])
 {
@@ -42,8 +125,31 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    for (int i = 0; i < MAX_SHM_SIZE; i+=4) {
-        printf("i:%d addr: %d\r\n", i, addr[i/4]);
+    int semid = __sem_init(1);
+    if (semid < 0) {
+        perror("sem init fail");
+        return -1;
+    }
+
+    int count = 0;
+    while (1) {
+        if (__sem_wait(semid) < 0) {
+            break;
+        }
+
+        __sem_get_value(semid);
+        for (int i = 0; i < MAX_SHM_SIZE; i+=4) {
+            printf("i:%d addr:%d ", i, addr[i/4]);
+        }
+        // sleep(4);
+        printf("\r\n");
+        printf("get count:%d\r\n", count++);
+    }
+
+    int ret = shmdt(addr);
+    if (ret < 0) {
+        perror("del shareMem fail");
+        return -1;
     }
 
     return 0;
--- posix/ipc/shareMem/shareMem_send.c
+++ posix/ipc/shareMem/shareMem_send.c
@@ -10,8 +10,94 @@
  */
 // #define _GNU_SOURCE
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
+#include <signal.h>
+
+static int __sem_init(int recv_flag)
+{
+#define SEM_BASE_PATH "../sem/semrcv.c"
+#define SEM_BASE_ID (87654321)
+    int semid;
+    key_t semkey = ftok(SEM_BASE_PATH, SEM_BASE_ID);
+    if (semkey == -1) {
+        perror("semkey get fail");
+        return -1;
+    }
+
+    if (!recv_flag) {
+        semid = semget(semkey, 1, IPC_CREAT | 0666);
+        if (semid < 0) {
+            perror("semget fail");
+            return -1;
+        }
+
+        union semun
+        {
+            int val;
+            struct semid_ds *buf;
+            unsigned short int *array;
+            struct seminfo *__buf;
+        } set;
+        set.val = 0;
+        int ret = semctl(semid, 0, SETVAL, set);
+        if (ret < 0) {
+            perror("semctl fail");
+            return -1;
+        }
+    } else {
+        semid = semget(semkey, 1, 0666);
+        if (semid < 0) {
+            perror("semget fail");
+            return -1;
+        }
+    }
+
+    return semid;
+}
+
+static int __sem_wait(int semid)
+{
+    struct sembuf buf = {
+        .sem_flg = 0,
+        .sem_num = 0,
+        .sem_op = -1,
+    };
+    int ret = semop(semid, &buf, 1);
+    if (ret < 0) {
+        perror("semop wait fail");
+        return -1;
+    }
+    return 0;
+}
+
+static int __sem_post(int semid)
+{
+    struct sembuf buf = {
+        .sem_flg = 0,
+        .sem_num = 0,
+        .sem_op = 1,
+    };
+    int ret = semop(semid, &buf, 1);
+    if (ret < 0) {
+        perror("semop post fail");
+        return -1;
+    }
+    
+    return 0;
+}
+
+static void __sem_get_value(int semid)
+{
+    int semvalue = semctl(semid, 0, GETVAL);
+    if (semvalue < 0) {
+        perror("semvalue get fail");
+        return ;
+    }
+    printf("semvalue is %d\r\n", semvalue);
+}
 
 static void get_shm_stat(int shmid)
 {
@@ -28,8 +114,9 @@ static void get_shm_stat(int shmid)
 }
 
 #define MSG_BASE_PATH "./shareMem_send.c"
-#define MSG_BASE_ID (12345678)
-#define MAX_SHM_SIZE (4096)
+#define MSG_BASE_ID (123456791)
+#define MAX_SHM_SIZE (4)
+
 
 int main(int argc, char *argv[])
 {
@@ -47,15 +134,38 @@ int main(int argc, char *argv[])
 
     get_shm_stat(shmid);
 
+
     int *addr = shmat(shmid, 0, 0);
     if (NULL == addr) {
         perror("addr NULL");
         return -1;
     }
-    
-    for (int i = 0; i < MAX_SHM_SIZE; i+=4) {
-        addr[i/4] = i;
+
+    int semid = __sem_init(0);
+    if (semid < 0) {
+        return -1;
     }
+
+    int count = 0;
+    while (1) {
+        for (int i = 0; i < MAX_SHM_SIZE; i+=4) {
+            addr[i/4] = count;
+            count++;
+        }
+        if (__sem_post(semid) < 0) {
+            break;
+        }
+
+        __sem_get_value(semid);
+        usleep(100);
+    }
+
+    int ret = shmdt(addr);
+    if (ret < 0) {
+        perror("del shareMem fail");
+        return -1;
+    }
+
     printf("get shmid: %d\r\n", shmid);
 
     return 0;
